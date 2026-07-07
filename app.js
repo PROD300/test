ac2b196091b7f794b76a3fbc625309df84d7beb8
PROD300 <dasha.zhelo@gmail.com>
2026-07-07 23:13:43 +0300

    Update app files

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+Hello! My name is ${FirstName} ${LastName}. I am ${age} years old and I live in ${city}. I am ${isMarried ? 'married' : 'not married'}. My hobbies include ${hobbies.join(', ')}.   
 const FirstName = 'Daria';
 const LastName = 'Polyakova';
 const age = 36;
