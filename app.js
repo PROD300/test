8436537f22b9608fe8c531c052203d1a7bb754e7
PROD300 <dasha.zhelo@gmail.com>
2026-07-07 22:35:03 +0300

    Rename variables

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const FirstName = 'Daria';
 const LastName = 'Polyakova';
 const age = 36;
 const isMarried = true;
-const sity = 'Moscow';
-const hobies = ['reading', 'traveling', 'cooking']
+const city = 'Moscow';
+const hobbies = ['reading', 'traveling', 'cooking']
